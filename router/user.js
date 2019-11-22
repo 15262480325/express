113cb835ee40860a683e00cd7c4cf1567567f8bc
lufei <lufei@xiniunet.com>
2019-11-22 17:38:06 +0800

    添加注销用户接口

--- router/user.js
+++ router/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const secret = 'lufei928';
+let accountInfo = {};
 
 //连接本地数据库
 mongoose.connect('mongodb://localhost:27017/lufei' , (err) => {
@@ -21,7 +22,7 @@ const userSchema = new mongoose.Schema({
     password: String,
     email: String,
     address: String,
-    birthday: Date
+    birthday: Number
 });
 
 //创建视图
@@ -37,6 +38,7 @@ router.use((req, res, next) => {
             //若果解析成功,去查去数据库看token里的用户信息是否存在
             userModel.find({phone: data.phone, password: data.password}, (err, data) => {
                 if (err || data.length === 0) return res.status(403).json({meg: '无效的身份,请重新登录', success: false});
+                accountInfo = data[0];
                 next();
             })
         })
@@ -96,21 +98,24 @@ router.post('/login', (req, res) => {
 
 //编辑用户信息
 router.post('/update', (req, res) => {
-    //判断是否有手机号
-    if (!req.body.phone)  return res.json({meg: '手机号不能为空,请传入手机号', success: false});
-    userModel.findOne({phone: req.body.phone}, (err, data) => {
-        if (!data || Object.keys(data).length === 0) return res.json({msg: '没有此用户,请重试', success: false});
-        //从用户提交的数据中提取出跟数据库字段值不相等的内容,然后更新
-        let update = {};
-        Object.keys(req.body).forEach(item => {
-            if (req.body[item] !== data[item]) {
-                update[item] = req.body[item];
-            }
-        });
-        userModel.findByIdAndUpdate(data._id, update, (error, result) => {
-            if (error) return res.status(500).json({msg: '系统错误,请稍后重试', success: false});
-            return res.json({msg: '更新成功', success: true})
-        })
+    //从用户提交的数据中提取出跟数据库字段值不相等的内容,然后更新
+    let update = {};
+    Object.keys(req.body).forEach(item => {
+        if (req.body[item] !== accountInfo[item]) {
+            update[item] = req.body[item];
+        }
+    });
+    userModel.findByIdAndUpdate(accountInfo._id, update, (error, result) => {
+        if (error) return res.status(500).json({msg: '系统错误,请稍后重试', success: false});
+        return res.json({msg: '更新成功', success: true})
+    })
+})
+
+//注销用户
+router.post('/logoff', (req, res) => {
+    userModel.findByIdAndRemove(accountInfo._id, {}, (err, data) => {
+        if (err) return res.status(500).json({meg: '系统错误,请稍后重试', success: false});
+        res.json({meg: '用户注销成功', success: true});
     })
 })
 
